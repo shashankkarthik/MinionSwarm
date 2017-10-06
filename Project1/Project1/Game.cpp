4764944cbdd2328db2a0199429391679e15cca39
Shashank Karthikeyan <karthik1@msu.edu>
2017-10-06 12:48:49 -0400

    Integrated scoreboard

--- Project1/Project1/Game.cpp
+++ Project1/Project1/Game.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <memory>
 #include "Game.h"
+#include <string>
 
 using namespace std;
 using namespace Gdiplus;
@@ -10,6 +11,11 @@ const static int Width = 1400;
 
 /// Game area height in virtual pixels
 const static int Height = 1100;
+
+
+const wstring JuicerImageName = L"images/juicer.png";
+const wstring PokeeballImageName = L"images/pokeball.png";
+const wstring AryaImageName = L"images/arya.png";
 
 CGame::CGame()
 {
@@ -50,6 +56,85 @@ void CGame::OnDraw(Gdiplus::Graphics *graphics, int width, int height)
 	// From here on you are drawing virtual pixels
 
 	mNewGameButton->Draw(graphics, Width, Height);
+
+
+	mJuicerImage = unique_ptr<Bitmap>(Bitmap::FromFile(JuicerImageName.c_str()));
+	if (mJuicerImage->GetLastStatus() != Ok)
+	{
+		wstring msg(L"Failed to open ");
+		msg += JuicerImageName;
+		AfxMessageBox(msg.c_str());
+	}
+
+	mPokeeballImage = unique_ptr<Bitmap>(Bitmap::FromFile(PokeeballImageName.c_str()));
+	if (mPokeeballImage->GetLastStatus() != Ok)
+	{
+		wstring msg(L"Failed to open ");
+		msg += PokeeballImageName;
+		AfxMessageBox(msg.c_str());
+	}
+
+	mAryaImage = unique_ptr<Bitmap>(Bitmap::FromFile(AryaImageName.c_str()));
+	if (mAryaImage->GetLastStatus() != Ok)
+	{
+		wstring msg(L"Failed to open ");
+		msg += AryaImageName;
+		AfxMessageBox(msg.c_str());
+	}
+
+	DrawTime(graphics, L"0:00");
+
+	graphics->DrawImage(mJuicerImage.get(), 650, -400);
+	DrawScore(graphics, 666.0f, -250.0f, L"0");
+
+	graphics->DrawImage(mPokeeballImage.get(), 666, -200);
+	DrawScore(graphics, 666.0f, -145, L"0");
+
+	graphics->DrawImage(mAryaImage.get(), 625, -95);
+	DrawScore(graphics, 666.0f, 60, L"0");
+
+
+}
+
+
+
+void CGame::DrawScore(Gdiplus::Graphics * graphics, float xLoc, float yLoc, wstring score)
+{
+	score = score + L"\0";
+	WCHAR* string = (WCHAR *)score.c_str();
+	Gdiplus::Font myFont(L"Arial", 16);
+	RectF layoutRect(xLoc, yLoc, 50.0f, 50.0f);
+	StringFormat format;
+	format.SetAlignment(StringAlignmentCenter);
+	SolidBrush greenBrush(Color(255, 0, 255, 0));
+
+	graphics->DrawString(
+		string,
+		-1,
+		&myFont,
+		layoutRect,
+		&format,
+		&greenBrush);
+}
+
+void CGame::DrawTime(Gdiplus::Graphics * graphics, wstring time)
+{
+	time = time + L"\0";
+	WCHAR* string = (WCHAR *)time.c_str();
+	Gdiplus::Font myFont(L"Arial", 20);
+	RectF layoutRect(650.0f, -450.0f, 80.0f, 50.0f);
+	StringFormat format;
+	format.SetAlignment(StringAlignmentCenter);
+	SolidBrush greenBrush(Color(255, 0, 255, 0));
+
+	graphics->DrawString(
+		string,
+		-1,
+		&myFont,
+		layoutRect,
+		&format,
+		&greenBrush);
+
 }
 
 /** Accept a visitor for the collection
--- Project1/Project1/Game.h
+++ Project1/Project1/Game.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <string>
 #include "GameVisitor.h"
 #include "NewGame.h"
 
@@ -15,11 +16,20 @@ public:
 
 	void Accept(CGameVisitor * visitor);
 
+	void CGame::DrawScore(Gdiplus::Graphics * graphics, float xLoc, float yLoc, std::wstring score);
+
+	void CGame::DrawTime(Gdiplus::Graphics * graphics, std::wstring score);
+
 
 private:
 	float mScale = 0;
 	float mXOffset = 0;
 	float mYOffset = 0;
+
+	std::unique_ptr<Gdiplus::Bitmap> mJuicerImage;
+	std::unique_ptr<Gdiplus::Bitmap> mPokeeballImage;
+	std::unique_ptr<Gdiplus::Bitmap> mAryaImage;
+
 	/// All of the items to populate our aquarium
 	std::shared_ptr<CNewGame> mNewGameButton;
 	///collection of items
