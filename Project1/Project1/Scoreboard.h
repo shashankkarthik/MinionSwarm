a2be17e60bae05e05a933173f41ec5271d383bba
Shashank Karthikeyan <karthik1@msu.edu>
2017-10-12 22:40:35 -0400

    Completed scoreboard.h

--- Project1/Project1/Scoreboard.h
+++ Project1/Project1/Scoreboard.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <memory>
+#include <string>
+
+using namespace std;
+using namespace Gdiplus;
 class CScoreboard
 {
 public:
 	CScoreboard();
-	~CScoreboard();
+	virtual	~CScoreboard();
+	void CScoreboard::DrawScore(Gdiplus::Graphics * graphics, float xLoc, float yLoc, std::wstring score);
+	void CScoreboard::DrawImage(Gdiplus::Graphics * graphics, unique_ptr<Bitmap> image, float xLoc, float yLoc);
+private:
+	unique_ptr<Gdiplus::Bitmap> mJuicerImage;
+	unique_ptr<Gdiplus::Bitmap> mPokeeballImage;
+	unique_ptr<Gdiplus::Bitmap> mAryaImage;
+
+	int mJuicerScore;
+	int mPokeeballScore;
+	int mArayaScore;
+
+
 };
 
